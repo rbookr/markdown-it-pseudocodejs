40c96eb913929a696806a68e50f8e12b6cdc7c49
rainboy <rainboylvx@qq.com>
2025-09-08 08:59:22 +0800

    fix: update multiple code sections with recent adjustments

--- browser.js
+++ browser.js
@@ -1,16 +1,32 @@
 var md = require('markdown-it')(),
 	mpjs = require('./index');
 
-md.use(mpjs);
+var _ = require('lodash');
+
+md.use(mpjs,{lineNumber:true});
 
 var input = document.getElementById('input'),
 	output = document.getElementById('output'),
 	button = document.getElementById('button');
+	readme = document.getElementById('readme');
+	readme_rendered = document.getElementById('readme_rendered');
 
-button.addEventListener('click', function(ev){
+const _readme_rendered = md.render(readme.textContent);
+console.log("----------------------")
+console.log(_readme_rendered);
+readme_rendered.innerHTML = _readme_rendered;
 
+// Function to render the input
+function renderInput() {
 	var result = md.render(input.value);
-
 	output.innerHTML = result;
+}
 
-});
+// Using lodash debounce to limit the rate at which the function can fire
+const debouncedRender = _.debounce(renderInput, 300); // 300ms delay
+
+// Add event listener for input changes
+input.addEventListener('input', debouncedRender);
+
+// Initial render if there's any content
+renderInput();
